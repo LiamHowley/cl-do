9e2a770c16fd573bf26e7cba1f5e56343630a647
Liam Howley <liam.howley@hotmail.com>
2021-06-23 10:40:14 +0100

    package.lisp for package. request functions moved to request.lisp

--- package.lisp
+++ package.lisp
@@ -2,44 +2,46 @@
   (:use :cl)
   (:import-from :drakma
 		:http-request)
-  (:import-from :puri
-		:uri)
   (:import-from :yason
 		:parse
 		:*parse-object-as*
-		:*parse-json-arrays-as-vectors*
-		:encode-plist)
-  (:export :list-all-droplets
-	   :list-all-droplets-by-tag
+		:*parse-object-key-fn*
+		:*symbol-key-encoder*)
+  (:export :list-droplets
 	   :create-droplet
-	   :create-multiple-droplets))
+	   :create-multiple-droplets
+	   :delete-droplet-by-tag
+	   :retrieve-droplet
+	   :delete-droplet
+	   :list-backups-for-a-droplet
+	   :list-snapshots-for-a-droplet
+	   :list-kernels-for-a-droplet
+	   :list-firewalls-for-a-droplet
+	   :list-neighbours-for-a-droplet
+	   :list-associated-resources-for-a-droplet
+	   :delete-droplet-and-resources
+	   :destroyed-droplet-status
+	   :retry-droplet-destroy
+	   :purge-droplet
+	   :droplet-neighbours
+
+	   ;;images
+	   :list-images
+	   :create-custom-image
+	   :retrieve-image
+	   :update-image
+	   :delete-image
+
+	   ))
 
 
 (in-package :cl-do)
 
 (defparameter *auth-token*
-  (with-open-file (stream #P "~/do" :direction :input) 
-    (let ((seq (make-array (file-length stream) :element-type 'character)))
-      (read-sequence seq stream)
-      seq)))
+  (concatenate 'string "Bearer " 
+	       (with-open-file (stream #P "~/do" :direction :input) 
+		 (let ((seq (make-array (file-length stream) :element-type 'character)))
+		   (read-sequence seq stream)
+		   seq))))
 
-(defun parse-response (response)
-  (let* ((length (array-total-size response))
-	 (json-string (make-array length :element-type 'character)))
-    (loop for count from 0 below length
-       for char = (code-char (aref response count))
-       when char
-       do (setf (char json-string count) char))
-    (yason:parse json-string)))
-       
-(defun get-request (url)
-  (http-request "https://api.digitalocean.com/v2/droplets"
-		:content-type "application/json"
-		:additional-headers `(("Authorization" . ,(concatenate 'string "Bearer " *auth-token*)))))
-
-(defun post-request (url data)
-  (http-request "https://api.digitalocean.com/v2/droplets"
-		:content-type "application/json"
-		:method "post"
-		:content data
-		:additional-headers `(("Authorization" . ,(concatenate 'string "Bearer " *auth-token*)))))
+(defparameter *base-uri* "https://api.digitalocean.com/v2/")
